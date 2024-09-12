ded60013363e0fb1b771eda2c390837710cf5ae7
kohki miyama <kmx.111010@gmail.com>
2024-09-12 20:57:30 +0900

    ステータスの追加

--- test/test.py
+++ test/test.py
@@ -1,7 +1,9 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, age, height, weight):
         self.name = name
-        self.age = 0
+        self.age = age
+        self.height = height
+        self.weight = weight
 
     def greet(self):
         return f"Hello, {self.name}! i am {self.age} years old."
