f359d8141a509f92b48169d25af90d8a47849df9
kohki miyama <kmx.111010@gmail.com>
2024-09-12 20:54:12 +0900

    年齢のステータス追加

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 class Person:
     def __init__(self, name):
         self.name = name
+        self.age = 0
 
     def greet(self):
-        return f"Hello, {self.name}!"
+        return f"Hello, {self.name}! i am {self.age} years old."
