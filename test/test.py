d8cdeedac523c704f33710a6dc793d5009e315e4
kohki miyama <kmx.111010@gmail.com>
2024-09-12 20:58:13 +0900

    成長に関するメソッドを追加

--- test/test.py
+++ test/test.py
@@ -7,3 +7,16 @@ class Person:
 
     def greet(self):
         return f"Hello, {self.name}! i am {self.age} years old."
+
+    def grow(self):
+        self.age += 1
+        if self.age < 10:
+            self.height += 2
+        elif self.age < 20:
+            self.height += 1
+
+    def eat(self):
+        self.weight += 1
+
+    def exercise(self):
+        self.weight -= 1
